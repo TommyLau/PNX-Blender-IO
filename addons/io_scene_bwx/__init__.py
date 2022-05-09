4715d159cb9999449ecb8bd1e0a8ad560042d34f
Tommy Lau <tommy.lhg@gmail.com>
2022-05-10 03:23:08 +0800

    Can import static model without texture nor animation

--- addons/io_scene_bwx/__init__.py
+++ addons/io_scene_bwx/__init__.py
@@ -24,30 +24,17 @@ bl_info = {
     'category': 'Import-Export',
 }
 
-
 #
 # Script reloading (if the user calls 'Reload Scripts' from Blender)
 #
-
-def reload_package(module_dict_main):
-    import importlib
-    from pathlib import Path
-
-    def reload_package_recursive(current_dir, module_dict):
-        for path in current_dir.iterdir():
-            if "__init__" in str(path) or path.stem not in module_dict:
-                continue
-
-            if path.is_file() and path.suffix == ".py":
-                importlib.reload(module_dict[path.stem])
-            elif path.is_dir():
-                reload_package_recursive(path, module_dict[path.stem].__dict__)
-
-    reload_package_recursive(Path(__file__).parent, module_dict_main)
-
-
 if "bpy" in locals():
-    reload_package(locals())
+    import importlib
+    if "bwx_construct" in locals():
+        importlib.reload(bwx_construct)
+    if "bwx_io" in locals():
+        importlib.reload(bwx_io)
+    if "bwx_blender" in locals():
+        importlib.reload(bwx_blender)
 
 import bpy
 from bpy.props import (StringProperty,
--- addons/io_scene_bwx/bwx_blender.py
+++ addons/io_scene_bwx/bwx_blender.py
@@ -24,7 +24,46 @@ class BlenderBWX:
     def create(bwx):
         """Create BWX main worker method."""
         BlenderBWX.prepare_data(bwx)
-        # BlenderScene.create(bwx)
+
+        for o in bwx.model:
+            [name, material, meshes, matrices] = o
+            [sub_material, positions, tex_coords, indices] = meshes[0]
+            faces = [indices[i:i + 3] for i in range(0, len(indices), 3)]
+
+            me = bpy.data.meshes.new(name)
+            me.from_pydata(positions, [], faces)
+            corrections = me.validate(verbose=True, clean_customdata=True)
+
+            uv_layer = me.uv_layers.new(do_init=False)  # Returns the created uv layer
+            vert_uvs = tex_coords
+            uv_layer.data.foreach_set("uv",
+                                      [uv for pair in [vert_uvs[l.vertex_index] for l in me.loops] for uv in pair])
+
+            # Try matrix
+            [timeline, matrix] = matrices[0]
+            mat = Matrix()
+            mat[0][0] = matrix[0]
+            mat[0][1] = matrix[4]
+            mat[0][2] = matrix[8]
+            mat[0][3] = matrix[12]
+            mat[1][0] = matrix[1]
+            mat[1][1] = matrix[5]
+            mat[1][2] = matrix[9]
+            mat[1][3] = matrix[13]
+            mat[2][0] = matrix[2]
+            mat[2][1] = matrix[6]
+            mat[2][2] = matrix[10]
+            mat[2][3] = matrix[14]
+            mat[3][0] = matrix[3]
+            mat[3][1] = matrix[7]
+            mat[3][2] = matrix[11]
+            mat[3][3] = matrix[15]
+
+            me.transform(mat)
+            me.calc_normals()
+            me.update()
+            new_object = bpy.data.objects.new(name, me)
+            bpy.context.collection.objects.link(new_object)
 
     @staticmethod
     def prepare_data(bwx):
