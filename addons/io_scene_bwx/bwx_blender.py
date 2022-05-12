803ad8460a339326fe451a9855b1b014c37e19a9
Tommy Lau <tommy.lhg@gmail.com>
2022-05-12 17:37:18 +0800

    Optimize matrix set up for mesh

--- addons/io_scene_bwx/bwx_blender.py
+++ addons/io_scene_bwx/bwx_blender.py
@@ -51,31 +51,19 @@ class BWXBlender:
             uv_layer.data.foreach_set("uv",
                                       [uv for pair in [vert_uvs[l.vertex_index] for l in me.loops] for uv in pair])
 
-            # Try matrix
-            [timeline, matrix] = matrices[0]
-            mat = Matrix()
-            mat[0][0] = matrix[0]
-            mat[0][1] = matrix[4]
-            mat[0][2] = matrix[8]
-            mat[0][3] = matrix[12]
-            mat[1][0] = matrix[1]
-            mat[1][1] = matrix[5]
-            mat[1][2] = matrix[9]
-            mat[1][3] = matrix[13]
-            mat[2][0] = matrix[2]
-            mat[2][1] = matrix[6]
-            mat[2][2] = matrix[10]
-            mat[2][3] = matrix[14]
-            mat[3][0] = matrix[3]
-            mat[3][1] = matrix[7]
-            mat[3][2] = matrix[11]
-            mat[3][3] = matrix[15]
-
-            # me.transform(mat)
             me.calc_normals()
             me.update()
             new_object = bpy.data.objects.new(name, me)
-            new_object.matrix_basis = mat
+
+            # Try matrix
+            if matrices:
+                [timeline, matrix] = matrices[0]
+                mat = Matrix()
+                for i in range(4):
+                    mat.col[i] = [matrix[j] for j in range(i * 4, (i + 1) * 4)]
+
+                new_object.matrix_basis = mat
+
             bpy.context.collection.objects.link(new_object)
 
     def prepare_data(self):
